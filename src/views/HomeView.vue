45d88ec99528ad290d09bffa489e223d8ca876f2
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-07-28 09:55:13 +0200

    Save progress.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,21 +5,37 @@ const id = ref('');
 const clientToken = ref('');
 const amount = ref('');
 
+const output = ref('');
+
 // On submit, make a post request to the server
 const submit = async () => {
-  const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/checkout`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      id: id.value,
-      clientToken: clientToken.value,
-      amount: amount.value,
-    }),
-  });
-  const data = await response.json();
-  console.log(data);
+  console.log(`Authorization : Bearer ${id.value}:${clientToken.value}`);
+  try {
+    const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/transactions`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Bearer': `${id.value}:${clientToken.value}`,
+      },
+      body: JSON.stringify({
+        id: id.value,
+        merchantId: id.value,
+        amount: amount.value,
+        userId: 1,
+        currency: 'USD',
+      }),
+    });
+    console.log(response.status);
+    const data = await response.json();
+    console.log(data); 
+    output.value = data;
+  } catch (error) {
+    output.value = error;
+    console.error(error);
+    console.log(id.value);
+    console.log(clientToken.value);
+    console.log(amount.value);
+  }
 };
 
 </script>
@@ -30,14 +46,15 @@ const submit = async () => {
       <v-text-field
         v-model="id"
         label="Id"
-        type="number"
-        maxlength="1"
+        type="text"
+        placeholder="3d8047b9-9cc1-407a-861a-bd1b48595b5d"
         required
       ></v-text-field>
       <v-text-field
         v-model="clientToken"
         label="Client Token"
-        type="text"
+        type="textarea"
+        placeholder="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjNkODA0N2I5LTljYzEtNDA3YS04NjFhLWJkMWI0ODU5NWI1ZCIsIm1lcmNoYW50SWQiOiI4NDE5NjkzMC03N2RjLTQ5MjYtYmM5NC0xMThhNzQ2MTI3OTAiLCJpc0FkbWluIjpmYWxzZSwiaWF0IjoxNjkwNDk2MDAxLCJleHAiOjE3MjIwNTM2MDF9.6tA1UK7kfVzV5l8jxiRUTbj_IPeIQSuaFChu6sbC8lg"
         required
       ></v-text-field>
       <v-text-field
@@ -54,5 +71,6 @@ const submit = async () => {
         Submit
       </v-btn>
     </v-form>
+    <p>{{ output }}</p>
   </main>
 </template>
